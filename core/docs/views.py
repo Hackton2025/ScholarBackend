58cef5f223a05ab3fce18703898fd2922f869983
kaubarros-24 <martinsbarroskaua@gmail.com>
2025-06-05 19:52:10 -0300

    CHORE: Create ai model

--- core/docs/views.py
+++ core/docs/views.py
@@ -1,3 +1,4 @@
+import os
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,23 +7,18 @@ import requests
 
 class DocumentAPIView(APIView):
     def post(self, request):
-        content  = request.data.get('content')
-        title = request.data.get('title')
-        send_to = request.data.get('send_to')
-        
-        if not content or not title or not send_to:
-            return Response({"message": "Content, title and teacher email is required"}, status=status.HTTP_400_BAD_REQUEST)
+        content = request.data.get("content")
+        if not content:
+            return Response({"error": "Content is required"}, status=status.HTTP_400_BAD_REQUEST)
         try:
-            json = {
+            data = {
                 "message": content
             }
-            response = requests.post("http://localhost:8000/api/ai", json=json )
-            print(response)
-            if not response:
-                return Response({"message": "AI service is not available"}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-            data = response.json()
-            return Response({"message": data}, status=status.HTTP_200_OK)
-        except requests.exceptions.RequestException as e:
-            return Response({"message": f"An error occurred: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except Exception as e:
-            return Response({"message": f"An unexpected error occurred: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            response = requests.post("http://127.0.0.1:8080/api/ai", json=data)
+            if response.status_code != 200:
+                return Response({"error": "Failed to process the request"}, status=response.status_code)
+            return Response({"message": response}, status=status.HTTP_200_OK)
+        except requests.RequestException as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:  
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
